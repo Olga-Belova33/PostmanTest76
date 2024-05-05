6c9be52b71cd392c020fa4e68d587effe5ea7d8d
Olga Belova <lera2205@mail.ru>
2024-05-05 17:29:01 +0300

    added src

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData() {
+        String text = "Hello";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo(text));
+    }
+}
